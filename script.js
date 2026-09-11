07094a0de5a0704fb63e050e1b10978be31a1199
TE-HSIUN <d1s1a5w3r5g3@gmail.com>
2026-09-11 17:54:34 +0800

    Remake init

--- script.js
+++
@@ -1,6 +0,0 @@
-import { initNavigation } from '/js/navigation.js';
-import { initMotion } from '/js/motion.js';
-
-// Independent enhancements; the static HTML remains fully readable without JS.
-initNavigation();
-initMotion();
